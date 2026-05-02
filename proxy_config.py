a3bb6d430925465a72342aa04572db2e7d0c9da0
Taha Tehrani Nasab <115104136+tahatehranin@users.noreply.github.com>
2026-05-02 14:00:32 +0330

    Create proxy_config.py

---
+++ proxy_config.py
@@ -0,0 +1,10 @@
+MIRRORS = [
+    "https://docker.iranserver.com",
+    "https://docker.abrha.net",
+    "https://docker.arvancloud.ir",
+    "https://mirror2.chabokan.net",
+    "https://docker.derak.cloud",
+]
+
+HEALTH_CHECK_INTERVAL = 60  # ثانیه
+HEALTH_CHECK_PATH = "/v2/"  # می‌توانید از "/health" هم استفاده کنید
